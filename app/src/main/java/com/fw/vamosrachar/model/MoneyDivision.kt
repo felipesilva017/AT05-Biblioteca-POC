8dc455dc5ddbb6fcc7084afe51a644c8e33e1082
Wesley Alves Rolim <walvesrolim@gmail.com>
2025-05-11 08:16:42 -0300

    ♻️ add validation to `MoneyDivision` properties

--- app/src/main/java/com/fw/vamosrachar/model/MoneyDivision.kt
+++ app/src/main/java/com/fw/vamosrachar/model/MoneyDivision.kt
@@ -1,6 +1,11 @@
 package com.fw.vamosrachar.model
 
 class MoneyDivision(private val totalMoney: Float, private val amountOfPeopleToDivide: Int) {
+    init {
+        require(totalMoney >= 0) { "totalMoney must not be less than zero." }
+        require(amountOfPeopleToDivide > 0) { "amountOfPeopleToDivide must not be zero." }
+    }
+
     fun calcMoneyForEachPerson(): Float {
         return totalMoney / amountOfPeopleToDivide
     }
